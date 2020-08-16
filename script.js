10ccca10c4d4e9098c2b36231a18633ab345fc2e
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-08-15 20:04:50 -0600

    still works

--- script.js
+++ script.js
@@ -2,33 +2,26 @@ const timerEl = document.getElementById('timer');
 const timerCl = document.querySelector('.timer');
 const startBtn = document.getElementById('start-btn');
 const questionContainerEl = document.getElementById('question-container');
+const instructionsEl = document.getElementById('instructions')
 const questionEl = document.getElementById('question');
 const answerBtnEl = document.getElementById('answer-buttons');
 const answerBtn1 = document.getElementById('Answer1');
 const answerBtn2 = document.getElementById('Answer2');
 const answerBtn3 = document.getElementById('Answer3');
 const answerBtn4 = document.getElementById('Answer4');
-const rightWrong = document.getElementById('right-or-wrong');
 const scoreEl = document.getElementById('score');
 const results = document.getElementById('results');
-const submitEl = document.getElementById('submit');
-const initialsEl = document.getElementById('initials');
 const highScoreEl = document.getElementById('high-score');
 
-
-
 // timer
-let timerCount = 50;
+let timerCount = 75;
 let timerId = '';
-// const h2El = document.createElement('h2');
-
 
 function countDown() {
     timerEl.innerText = timerCount;
     if (timerCount > 0) {
         timerCount--;
     } else {
-        console.log('time is up');
         clearInterval(timerId);
         questionContainerEl.classList.add('hide');
         timerCl.textContent = 'Time is up! Quiz will reload!';
@@ -44,13 +37,12 @@ countDown()
 startBtn.addEventListener('click', startGame)
 
 function startGame() {
-    console.log('started')
+
     clearInterval(timerId);
     startBtn.classList.add('hide')
     questionContainerEl.classList.remove('hide')
 
     displayQuestion()
-    console.log('question index', curQuestionIndex)
     timerId = setInterval(countDown, 1000)
 }
 
@@ -58,7 +50,6 @@ function startGame() {
 let curQuestionIndex = 0
 
 function displayQuestion() {
-
     questionEl.innerText = questions[curQuestionIndex].question
     answerBtn1.innerText = questions[curQuestionIndex].answers[0].text
     answerBtn2.innerText = questions[curQuestionIndex].answers[1].text
@@ -68,11 +59,9 @@ function displayQuestion() {
     answerBtn2.setAttribute('data-correct', questions[curQuestionIndex].answers[1].correct)
     answerBtn3.setAttribute('data-correct', questions[curQuestionIndex].answers[2].correct)
     answerBtn4.setAttribute('data-correct', questions[curQuestionIndex].answers[3].correct)
-    // rightWrong.innerText = ''
 }
 
 // select answer
-
 let answerCorrect = 0
 let answerWrong = 0
 
@@ -83,18 +72,12 @@ answerBtn4.addEventListener('click', clickAnswer)
 
 function clickAnswer() {
     let selection = this.getAttribute('data-correct')
-    console.log('begin', timerCount, timerId)
-    console.log(selection)
     if (selection == 'true') {
         answerCorrect++
-        console.log('correct')
-        // rightWrong.innerText = 'You got it right!'
         alert('You got it right!')
     } else {
         answerWrong++
         timerCount -= 10
-        console.log('wrong', timerCount, timerId)
-        // rightWrong.innerText = 'OOPS, you missed this one.'
         alert('OOPS, you missed this one.')
     }
     if (curQuestionIndex < questions.length - 1) {
@@ -106,54 +89,23 @@ function clickAnswer() {
         endGame()
     }
 }
-// const scores = []
-// const user = []
 
 function endGame() {
     clearInterval(timerId)
     timerEl.innerText = timerCount
-    console.log('timer id', timerId)
-    console.log('timer count', timerCount)
     questionContainerEl.classList.add('hide')
+    instructionsEl.classList.add('hide')
     scoreEl.classList.remove('hide')
     let score = (timerCount * answerCorrect)
-    results.innerText = "Right: " + answerCorrect + " / Wrong: " + answerWrong + " / Score: " + score
+    results.innerText = "Right: " + answerCorrect + " / Wrong: " + answerWrong + " / Your Score: " + score
 
     let highScore = parseInt(localStorage.getItem('highScore'));
-    console.log('high score', highScore);
-
     highScoreEl.textContent = highScore || 0;
 
     if (highScore && score > highScore) {
         localStorage.setItem('highScore', score);
     } else if (!highScore) localStorage.setItem('highScore', score);
-
-    // localStorage.setItem('score', score)
-    // scores.push(score);
-    // console.log(scores)
-    // localStorage.setItem('score', JSON.stringify(scores));
 }
-
-// function saveUser() {
-//     let initials = initialsEl.value
-//     console.log(initialsEl.value)
-//     localStorage.setItem('initials', initials);
-// }
-
-// submitEl.addEventListener('click', saveUser)
-
-// saveUser()
-
-// function saveUser() {
-//     let initials = initialsEl.value
-//     user.push(initials);
-//     console.log(user)
-//     localStorage.setItem('initials', JSON.stringify(user))
-
-//     scores.push(score);
-//     console.log(scores)
-//     localStorage.setItem('score', JSON.stringify(scores))
-// }
 
 const questions = [
     {
